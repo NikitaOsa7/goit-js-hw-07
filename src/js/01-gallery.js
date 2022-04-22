f04b30c3efc8645adbc5b735f8196d36f2247dea
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-04-22 15:57:20 +0300

    повторение

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const makeGalleryItemMarkup = ({ preview, original, description }) => {
-  return `
+const makeGalleryItemMarkup = ({preview, original, description}) => {
+   return `
  <div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -16,54 +16,55 @@ const makeGalleryItemMarkup = ({ preview, original, description }) => {
     `;
 };
 
-const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('');
+
+const makeGalleryMarkup = galleryItems.map(makeGalleryItemMarkup).join('')
 
 const galleryElements = document.querySelector('.gallery');
 
 galleryElements.insertAdjacentHTML('beforeend', makeGalleryMarkup);
 
-galleryElements.addEventListener('click', onGalleryClick);
+galleryElements.addEventListener('click', onModalShow);
 
-function onGalleryClick(event) {
+function onModalShow(event) {
   if (event.target.nodeName !== 'IMG') {
     return;
   }
   event.preventDefault();
-  modalShow(event.target.dataset.source);
-}
+  modalShow(event.target.dataset.source)
+};
+
 let instance;
+
 function modalShow(src) {
-  instance = basicLightbox.create(
-    `
-    <div class="modal">
-        <img src="${src}" style="height:100vh; display:block"></img>
-    </div>
+  instance = basicLightbox.create(`
+  <div class="modal">
+    <img src="${src}" style="height:100vh; display:block"></img>
+   </div>
 `,
-    {
+  
+  {
       onShow: instance => {
-        addListener();
+        addListener()
       },
-      onClose: instance => {
-        removeListener();
+    onclose: instance => {
+        removeListener()
       },
     },
   );
   instance.show();
-}
+};
+
 
 function addListener() {
-  window.addEventListener('keydown', onEscClick);
-}
+  window.addEventListener('keydown', onEscClick)
+};
+
+function removeListener() {
+  window.removeEventListener('keydown', onEscClick)
+};
 
 function onEscClick(event) {
   if (event.code === 'Escape') {
     instance.close();
   }
-}
-
-function removeListener() {
-  window.removeEventListener('keydown', onEscClick);
-}
-
-
-console.log(galleryItems);
+};
